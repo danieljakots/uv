3d5c0802117738c9b5fcfa6d387317dea9f35f66
Daniel Jakots <danj@chown.me>
2021-10-16 20:20:46 -0400

    Remove everything about migration
    I won't use in short/mid term. If I need it again, I'll get it from
    the git history.

--- uv.py
+++ uv.py
@@ -8,7 +8,6 @@ import argparse
 # XXX remove os with subprocess.run
 import os
 import re
-import socket
 import subprocess
 import sys
 import time
@@ -16,7 +15,6 @@ import uuid
 
 import jinja2
 import libvirt
-import paramiko
 
 DD_BS = 4096
 ZSTD_LEVEL = 6
@@ -129,72 +127,6 @@ def check_logical_volume_on_local(logical_volume):
     return size
 
 
-def make_logical_volume_on_remote(ssh_client, logical_volume_name, logical_volume_size):
-    remote_cmd = [
-        "ssh",
-        "otherkvm",
-        f"lvs {logical_volume_name} -o LV_SIZE --noheadings --units B --nosuffix",
-    ]
-
-    remote_result = subprocess.run(
-        remote_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8"
-    )
-    if remote_result.returncode != 0:
-        print(f"I can't find {logical_volume_name} on remote")
-        lvcreate_cmd = (
-            f"lvcreate -L{logical_volume_size}B "
-            + f"-n{logical_volume_name.split('/')[-1]} ubuntu-vg"
-        )
-        print(lvcreate_cmd)
-        copy_remote_answer = input(
-            "Should I run this command on remote? ('yes' to confirm)\n"
-        )
-        if copy_remote_answer != "yes":
-            sys.exit(3)
-        else:
-            stdin, stdout, stderr = ssh_client.exec_command(lvcreate_cmd)
-            # Recheck
-            remote_result = subprocess.run(
-                remote_cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-                encoding="utf-8",
-            )
-    local_size = logical_volume_size
-    remote_size = remote_result.stdout.strip()
-    if local_size != remote_size:
-        print(f"{logical_volume_name} is not the same size on local vs remote:")
-        print(f"{local_size}B on local VS {remote_size}B on remote")
-        sys.exit(3)
-
-
-def copy_logical_volume(logical_volume_name, logical_volume_size):
-    print(f"Copying {logical_volume_name}")
-    cmd = [
-        "dd",
-        "if=" + logical_volume_name,
-        "bs=" + str(DD_BS),
-        "|",
-        f"pv --size {str(logical_volume_size).strip()}",
-        "|",
-        "zstd",
-        f"-{ZSTD_LEVEL}",
-        "|",
-        "ssh",
-        "otherkvm",
-        f"'zstd -d | dd of={logical_volume_name} bs={str(DD_BS)}'",
-    ]
-    # subprocess.run does't seem to work :(
-    print(" ".join(cmd))
-    os.system(" ".join(cmd))
-
-
-def copy_definition(guest, ssh_client):
-    sftp_client = ssh_client.open_sftp()
-    sftp_client.put(f"/root/{guest}.xml", f"/root/{guest}.xml")
-    sftp_client.close()
-
-
 def shutdown_guest(guest, qemu_conn):
     qemu_conn.lookupByName(guest).shutdown()
     print("Guest has been shat down")
@@ -266,62 +198,10 @@ def inventary(qemu_conn):
     return guests
 
 
-def offline_migration(qemu_conn, ssh_client, guest, logical_volumes_dict):
-    # Shutdown the guest if it runs and wait until it's down
-    print(f"Moving {guest}")
-    guest_was_running = False
-    if is_guest_running(qemu_conn, guest):
-        guest_was_running = True
-        print("Guest is running, shutthing it down")
-        shutdown_guest(guest, qemu_conn)
-        wait_for_guest_down(guest, qemu_conn)
-    qemu_conn.close()
-
-    # Get the disk(s) on the other host
-    for logical_volume_name, logical_volume_size in logical_volumes_dict.items():
-        copy_logical_volume(logical_volume_name, logical_volume_size)
-
-    # Dump the guest definition and ship it to the other kvm
-    cmd = ["virsh", "dumpxml", guest]
-    with open(f"/root/{guest}.xml", "w") as f:
-        subprocess.run(cmd, stdout=f)
-    copy_definition(guest, ssh_client)
-
-    print(f"Defining {guest} on remote")
-    ssh_client.exec_command(f"virsh define /root/{guest}.xml")
-
-    # It might be too fast otherwise
-    time.sleep(1)
-    if guest_was_running:
-        print(f"Starting {guest} on remote")
-        ssh_client.exec_command(f"virsh start {guest}")
-
-    undefine_guest(guest)
-
-
 def undefine_guest(guest):
     print(f"Undefining {guest} on local")
     cmd = ["virsh", "undefine", guest]
     subprocess.run(cmd)
-
-
-def live_migration(guest):
-    # persistent will define the guest on remote
-    # undefinesource will undefine the guest on local
-    cmd = [
-        "virsh",
-        "migrate",
-        "--verbose",
-        "--live",
-        "--copy-storage-all",
-        "--persistent",
-        "--undefinesource",
-        guest,
-        "qemu+ssh://otherkvm/system",
-    ]
-    # Using that instead of subprocess.run to have the progress in real time
-    print("Running ", " ".join(cmd))
-    os.system(" ".join(cmd))
 
 
 def parse_cli():
@@ -359,17 +239,6 @@ def parse_cli():
     parser_start = subparsers.add_parser("start", help="Start an existing guest")
     parser_start.add_argument("guest", help="Name of the guest")
 
-    parser_move = subparsers.add_parser("move", help="Move an existing guest")
-    parser_move.add_argument("guest", help="Name of the guest")
-    group_move = parser_move.add_mutually_exclusive_group(required=True)
-    group_move.add_argument("--live", action="store_true")
-    group_move.add_argument("--offline", action="store_true")
-    parser_move.add_argument(
-        "--disable-bell",
-        action="store_true",
-        help="By default it will send a bell to the term once the migration is done",
-    )
-
     parser_stop = subparsers.add_parser(
         "stop", aliases=["shutdown"], help="Stop cleanly an existing guest"
     )
@@ -403,19 +272,6 @@ def does_guest_exist(known_guests, guest):
     return guest in known_guests.keys()
 
 
-def ssh_init():
-    # Try to connect, to check if the otherkvm is reachable
-    try:
-        ssh_client = paramiko.SSHClient()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname="otherkvm", username="root", timeout=3)
-    except (paramiko.ssh_exception.NoValidConnectionsError, socket.timeout):
-        print("NOPE, I can't ssh into the other kvm")
-        sys.exit(3)
-
-    return ssh_client
-
-
 def check_guest_exists_runs(qemu_conn, known_guests, guest, should_be_running):
     if not does_guest_exist(known_guests, guest):
         print(f"NOPE: guest {guest} not known")
@@ -440,31 +296,7 @@ def main():
     known_guests = inventary(qemu_conn)
     args = parse_cli()
 
-    if args.verb == "move":
-        time_begin = time.time()
-        ssh_client = ssh_init()
-
-        if not does_guest_exist(known_guests, args.guest):
-            print(f"NOPE: guest {args.guest} not known")
-            sys.exit(3)
-        # Check all the lv exist on remote
-        for lv_name, lv_size in known_guests[args.guest]["disks"].items():
-            print(f"Checking on remote {lv_name} (size {lv_size}B)")
-            make_logical_volume_on_remote(ssh_client, lv_name, lv_size)
-
-        if args.offline:
-            offline_migration(
-                qemu_conn, ssh_client, args.guest, known_guests[args.guest]["disks"]
-            )
-        elif args.live:
-            live_migration(args.guest)
-        if not args.disable_bell:
-            # print a bell to notify the migration is done
-            print("\a")
-        time_end = time.time()
-        total_time = int(time_end - time_begin)
-        print(f"Migration took {str(total_time)}s")
-    elif args.verb == "start":
+    if args.verb == "start":
         should_be_running = False
         check_guest_exists_runs(qemu_conn, known_guests, args.guest, should_be_running)
         start_guest(args.guest, qemu_conn)
